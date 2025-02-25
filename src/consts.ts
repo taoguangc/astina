a4cd0e3dd44f715020861442f2e55762bcc419a9
taoguangc <taoguangc@gmail.com>
2025-02-25 15:37:28 +0800

    fix font alert on vercel

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = '极客创业'
 export const SITE_DESCRIPTION = '专注于技术创新和创业'
-export const SITE_URL = 'https://geek-startup.com'
+export const SITE_URL = 'https://astina.vercel.app'
